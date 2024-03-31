a0ff00d611976cb2d2e509d02c2ff9b0501891b3
chip1911 <125810931+chip1911@users.noreply.github.com>
2024-03-31 15:48:12 +0700

    Update edit_order_management.php

--- edit_order_management.php
+++ edit_order_management.php
@@ -15,7 +15,7 @@
     }
 
 
-    $conn = new mysqli('localhost', 'root', 'Conanvskid1!', 'bookstore');
+    $conn = new mysqli('localhost', 'root', 'password', 'bookstore');
     if($conn->connect_error){
         die("Connect failed". mysqli_connect_error());
     }
